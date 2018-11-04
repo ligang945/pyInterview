85aa5da7264050dceec98f606817234ca599cd97
ligang <ligang945@gmail.com>
2018-11-04 20:51:19 +0800

    add more

---
+++ src/generator.py
@@ -0,0 +1,5 @@
+l = [x**2 for x in range(5)]
+print(l)
+
+g = (x**2 for x in range(5))
+print(list(g))
--- src/is_python_easy_or_not.py
+++ src/is_python_easy_or_not.py
@@ -1,7 +1,41 @@
+# 1
 class A(object):
     x = 1
     gen = (lambda x: (x for x in range(10)))(x)
 
 
 if __name__ == "__main__":
-    print(A.gen)
+    print(list(A.gen))
+
+
+# 2
+print('-' * 20)
+
+import time
+
+
+class Timeit(object):
+
+    def __init__(self, func):
+        self._wrapped = func
+
+    def __call__(self, *args, **kws):
+        start_time = time.time()
+        result = self._wrapped(*args, **kws)
+        print("elapsed time is %s " % (time.time() - start_time))
+        return result
+
+    def __get__(self, instance, owner):
+        return lambda *args, **kwargs: self.__call__(instance, *args, **kwargs)
+
+
+class A(object):
+
+    @Timeit
+    def func(self):
+        time.sleep(1)
+        print('invoking method func')
+
+a = A()
+a.func()
+
